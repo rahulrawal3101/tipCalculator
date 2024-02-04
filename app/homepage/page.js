055a1a2ba562b30465db5973323d2b5ce8be227a
Rahul Rawal <rahulrawal3101@gmail.com>
2024-02-04 23:56:53 +0530

    add functionality

--- app/homepage/page.js
+++ app/homepage/page.js
@@ -1,52 +1,52 @@
 'use client'
-import { Box, Button, Grid, InputBase, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import { Box, Button, Grid,  Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import PersonIcon from '@mui/icons-material/Person';
 
 const HomePage = () => {
-    const [billAmount, setBillAmount] = useState('');
-    const [person, setPerson] = useState('');
-    const [tipAmtPerPerson, setTipAmtPerPerson] = useState('0.00');
-    const [totalAmtPerPerson,setTotalAmountPerPerson] = useState('0.00');
-    const [customAmt,setCustomAmt] = useState('');
+    const [data,setData]=useState({billAmount:"",tipAmount:"",numOfPerson:"",totalTipPerPerson:"0.00",totalAmountPerPerson:"0.00"});
     
-    // console.log(billAmount,person)
-    // console.log(customAmt)
 
-    const addTipAmount=(tipAmt)=>{
-        // console.log(tipAmt);
-        if(person >= 0){
-            const billAmt = parseFloat(billAmount);
-        let tip;
-        if(tipAmt == 'custom'){
-            tip = parseFloat(customAmt);
-        }else{
-            tip = billAmt * (tipAmt /100);
-        }
-        const total = billAmt + tip;
-        const tipPerPerson = tip / person;
-        const amountperPerson = total / person;
-        setTipAmtPerPerson(tipPerPerson.toFixed(2));
-        setTotalAmountPerPerson(amountperPerson.toFixed(2));
-        }
-        if(person == 0){
-            alert('Please Choose the Person')
-        }
-        
-    };
 
-    const clearData=()=>{
-        setBillAmount('');
-        setPerson('');
-        setTipAmtPerPerson('0.00');
-        setTotalAmountPerPerson('0.00');
-        setCustomAmt('');
-    };
-
-    const handleCustomTipAmt=(e)=>{
-        setCustomAmt(e.target.value);
-        addTipAmount(parseFloat(e.target.value));
+    const addTipAmount=(num)=>{
+        setData({...data,tipAmount:Number(num)});
     }
+
+
+    const handleReset=()=>{
+        setData({...data,billAmount:'',tipAmount:0,numOfPerson:'',totalTipPerPerson:'0.00',totalAmountPerPerson:'0.00'});
+        location.reload();
+    }
+
+
+    const handleCalculate =()=>{
+        
+        
+
+        if(data.billAmount !=0 && data.tipAmount !=0 && data.numOfPerson != 0){
+            
+
+            const tip = (data.tipAmount/100)*data.billAmount;
+            const tipPerson=tip/data.numOfPerson;
+            const totalAmount = tip+data.billAmount;
+
+            const amountPerson = totalAmount/data.numOfPerson
+
+            // console.log("tip",tip)
+            // console.log("amount",tipPerson)
+            // console.log("totalAmount",totalAmount)
+            // console.log("amountPerson",amountPerson)
+
+            setData({...data,totalAmountPerPerson:amountPerson.toFixed(2),totalTipPerPerson:tipPerson.toFixed(2)});
+        }
+    }
+
+    useEffect(()=>{
+        handleCalculate();
+    },[data])
+    
+    // console.log("data check",data)
+  
     return (
         <Grid container sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <Grid item lg={6} md={9} sm={10} xs={12} sx={{ bgcolor: '#C5E4E7' }}>
@@ -62,7 +62,7 @@ const HomePage = () => {
                                 <Typography sx={{ color: '#6B7777', fontSize: '11px', fontFamily: 'monospace', fontWeight: 'bold' }}>Bill</Typography>
                                 <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', bgcolor: '#F3F8FB', borderRadius: '4px', mt: '5px' }}>
                                     <Typography sx={{ color: '#A9C3C6', p: '5px 0px 5px 5px', fontSize: '16px' }}>$</Typography>
-                                    <input placeholder='0' type='number' style={{ display: 'block', backgroundColor: 'transparent', outline: 'none', textAlign: 'end', border: '3px solid transparent', color: '#00474B', fontWeight: 'bold' }} value={billAmount} onChange={(e)=>{setBillAmount(e.target.value)}}/>
+                                    <input placeholder='0' type='number' style={{ display: 'block', backgroundColor: 'transparent', outline: 'none', textAlign: 'end', border: '3px solid transparent', color: '#00474B', fontWeight: 'bold' }} value={data.billAmount} onChange={(e)=>{setData({...data,billAmount:Number(e.target.value)})}}/>
 
                                 </Box>
 
@@ -90,20 +90,20 @@ const HomePage = () => {
                                     </Grid>
                                     <Grid item xs={3.1} sx={{ mt: '11px' }}>
 
-
-                                        <input placeholder='custom' type='number' style={{ display: 'block', backgroundColor: '#F3F8FB', outline: 'none', textAlign: 'end', border: '3px solid transparent', color: '#00474B', fontWeight: 'bold', width: '91%', p: '5px 8px' }} value={customAmt} onChange={handleCustomTipAmt}/>
+                                        {/* custom input is also calculate like a percentage  */}
+                                        <input placeholder='custom' type='number' style={{ display: 'block', backgroundColor: '#F3F8FB', outline: 'none', textAlign: 'end', border: '3px solid transparent', color: '#00474B', fontWeight: 'bold', width: '91%', p: '5px 8px' }} onChange={(e)=>{setData({...data,tipAmount:Number(e.target.value)})}}/>
 
                                     </Grid>
 
                                     <Grid item xs={12} sx={{ mt: '25px' }}>
                                         <Box sx={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
                                             <Typography sx={{ color: '#6B7777', fontSize: '11px', fontFamily: 'monospace', fontWeight: 'bold' }}>Number of People</Typography>
-                                            <Typography sx={{ color: '#B48372', fontSize: '11px', fontFamily: 'monospace', fontWeight: 'bold', }}>Can,t be zero</Typography>
+                                            <Typography sx={{ color: '#B48372', fontSize: '11px', fontFamily: 'monospace', fontWeight: 'bold',display:data.numOfPerson <=0 ? 'block':'none' }}>Can,t be zero</Typography>
                                         </Box>
                                         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', bgcolor: '#F3F8FB', borderRadius: '4px', mt: '5px' }}>
 
                                             <PersonIcon sx={{ color: '#A9C3C6', pl: '3px', fontSize: '20px' }} />
-                                            <input placeholder='0' type='number' style={{ display: 'block', backgroundColor: 'transparent', outline: 'none', textAlign: 'end', border: '3px solid transparent', color: '#00474B', fontWeight: 'bold' }} value={person} onChange={(e)=>{setPerson(e.target.value)}}/>
+                                            <input placeholder='0' type='number' style={{ display: 'block', backgroundColor: 'transparent', outline: 'none', textAlign: 'end', border: '3px solid transparent', color: '#00474B', fontWeight: 'bold' }} value={data.numOfPerson} onChange={(e)=>{setData({...data,numOfPerson:Number(e.target.value)})}}/>
 
                                         </Box>
 
@@ -112,7 +112,7 @@ const HomePage = () => {
 
                             </Grid>
 
-                            <Grid itemlg={5.7} md={5.7} sm={10} xs={12} sx={{ bgcolor: '#00474B', borderRadius: '7px', mt: { lg: '0px', md: '0px', sm: '20px', xs: '20px' } }}>
+                            <Grid item lg={5.7} md={5.7} sm={10} xs={12} sx={{ bgcolor: '#00474B', borderRadius: '7px', mt: { lg: '0px', md: '0px', sm: '20px', xs: '20px' } }}>
                                 <Grid container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: '30px' }}>
                                     <Grid item xs={10} >
                                         <Grid container>
@@ -122,7 +122,7 @@ const HomePage = () => {
                                                     <Typography sx={{ color: '#649BA0', fontSize: '11px', fontFamily: 'monospace', ml: '7px' }}>/Person</Typography>
                                                 </Box>
                                                 <Box>
-                                                    <Typography sx={{ color: '#21C3AC', fontFamily: 'monospace', fontSize: '21px', fontWeight: 'bold' }}>${tipAmtPerPerson}</Typography>
+                                                    <Typography sx={{ color: '#21C3AC', fontFamily: 'monospace', fontSize: '21px', fontWeight: 'bold' }}>${data.totalTipPerPerson }</Typography>
                                                 </Box>
                                             </Grid>
                                             <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', pt: '50px' }}>
@@ -131,13 +131,13 @@ const HomePage = () => {
                                                     <Typography sx={{ color: '#649BA0', fontSize: '11px', fontFamily: 'monospace', ml: '7px' }}>/Person</Typography>
                                                 </Box>
                                                 <Box>
-                                                    <Typography sx={{ color: '#21C3AC', fontFamily: 'monospace', fontSize: '21px', fontWeight: 'bold' }}>${totalAmtPerPerson}</Typography>
+                                                    <Typography sx={{ color: '#21C3AC', fontFamily: 'monospace', fontSize: '21px', fontWeight: 'bold' }}>${ data.totalAmountPerPerson}</Typography>
                                                 </Box>
                                             </Grid>
                                         </Grid>
                                         <Grid container sx={{ mt: '42px', mb: '45px' }}>
                                             <Grid item xs={12}>
-                                                <Button sx={{ color: '#004849', bgcolor: '#9FE8DF', fontSize: '12px', fontWeight: 'bold', width: '100%', '&:hover': { bgcolor: '#9FE8DF', color: '#004849' } }} onClick={clearData}>Reset</Button>
+                                                <Button sx={{ color: '#004849', bgcolor: '#9FE8DF', fontSize: '12px', fontWeight: 'bold', width: '100%', '&:hover': { bgcolor: '#9FE8DF', color: '#004849' } }} onClick={handleReset}>Reset</Button>
                                             </Grid>
 
                                         </Grid>
